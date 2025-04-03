46caead184c22ad6e56c5f127f6bd736486cbc05
Pavel Berezhnoy <p.berezhnoy@corp.mail.ru>
2025-04-03 13:06:08 +0300

    upgrade conduit && reimplement startup

--- conduit/main.go
+++ conduit/main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
 	sdk "github.com/conduitio/conduit-connector-sdk"
+	"github.com/conduitio/conduit/cmd/conduit/cli"
 	"github.com/conduitio/conduit/pkg/conduit"
 	proc_builtin "github.com/conduitio/conduit/pkg/plugin/processor/builtin"
 	mysql "github.com/derElektroBesen/conduit-connector-mysql"
 	postgres "github.com/derElektroBesen/conduit-connector-postgres"
 	"github.com/derElektroBesen/mysql-migrator/conduit/processor"
-	"gopkg.in/yaml.v2"
 )
-
-func readYAMLConfig(filename string, dest *conduit.Config) error {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return fmt.Errorf("unable to read file %q: %w", filename, err)
-	}
-
-	err = yaml.Unmarshal(data, &dest)
-	if err != nil {
-		return fmt.Errorf("unable to unmarshal config %q: %w", filename, err)
-	}
-	return nil
-}
 
 func newPSQLConnector(name string) sdk.Connector {
 	c := postgres.Connector
@@ -48,11 +31,6 @@ func main() {
 	// connectors
 	cfg := conduit.DefaultConfig()
 
-	// TODO: Merge config with command-lile arguments
-	if err := readYAMLConfig(cfg.ConduitCfg.Path, &cfg); err != nil {
-		log.Fatalf("unable read config: %s", err)
-	}
-
 	// Add the HTTP connector to list of built-in
 	// connectors
 	cfg.ConnectorPlugins["mysql"] = mysql.Connector
@@ -63,6 +41,5 @@ func main() {
 	// Setup builtin processor
 	proc_builtin.DefaultBuiltinProcessors["mysql-datatypes-processor"] = processor.NewMigrationProcessor
 
-	e := &conduit.Entrypoint{}
-	e.Serve(cfg)
+	cli.Run(cfg)
 }
